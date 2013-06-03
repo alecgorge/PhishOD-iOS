eff0825629461caa89d86dd2684694555b789406
Alec Gorge <alecgorge@gmail.com>
2013-06-03 13:38:58 -0400

    Initial Commit

---
+++ Phish Tracks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Phish Tracks
+//
+//  Created by Alec Gorge on 6/3/13.
+//  Copyright (c) 2013 Alec Gorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
