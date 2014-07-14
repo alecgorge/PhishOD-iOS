4bb79fdb6b3ac31c717c1d9586ba276ddaf92521
ruddfawcett <rudd.fawcett@gmail.com>
2014-07-14 00:40:49 -0400

    Created icon helper category on UIImage

---
+++ Phish Tracks/Supporting Files/Icons/UIImage+Icons.m
@@ -0,0 +1,49 @@
+//
+//  UIImage+Icons.m
+//  Phish Tracks
+//
+//  Created by Rudd Fawcett on 7/13/14.
+//  Copyright (c) 2014 Alec Gorge. All rights reserved.
+//
+
+#import "UIImage+Icons.h"
+
+@implementation UIImage (Icons)
+
++ (instancetype)forwardIcon {
+    return [UIImage imageNamed:@"forward-icon"];
+}
+
++ (instancetype)forwardIconWhite {
+    return [UIImage imageNamed:@"forward-icon-white"];
+}
+
++ (instancetype)heartIconWhite {
+    return [UIImage imageNamed:@"heart-icon-white"];
+}
+
++ (instancetype)pauseIcon {
+    return [UIImage imageNamed:@"pause-icon"];
+}
+
++ (instancetype)pauseIconWhite {
+    return [UIImage imageNamed:@"pause-icon-white"];
+}
+
++ (instancetype)playIcon {
+    return [UIImage imageNamed:@"play-icon"];
+}
+
++ (instancetype)playIconWhite {
+    return [UIImage imageNamed:@"play-icon-white"];
+}
+
++ (instancetype)rewindIcon {
+    return [UIImage imageNamed:@"rewind-icon"];
+}
+
++ (instancetype)rewindIconWhite {
+    return [UIImage imageNamed:@"rewind-icon-white"];
+}
+
+@end
