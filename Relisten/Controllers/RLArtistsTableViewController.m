4e625ec0e13e64e35b7b6924c336f920e9fe3360
George Lo <twgeolo@gmail.com>
2015-12-08 02:27:09 -0500

    Playback bar should show on artists listing

--- Relisten/Controllers/RLArtistsTableViewController.m
+++ Relisten/Controllers/RLArtistsTableViewController.m
@@ -86,6 +86,13 @@ static NSArray *featuredArtists;
     }
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [AppDelegate.sharedDelegate.navDelegate addBarToViewController: self];
+    [AppDelegate.sharedDelegate.navDelegate fixForViewController:self];
+}
+
 - (void)showSettings {
     UINavigationController *navController = [UINavigationController.alloc initWithRootViewController:RLSettingsViewController.new];
     
